bb55189aedf59287c7461320cb3651900793c91d
sayyadshahid <shahidsayyed0212@gmail.com>
2026-08-15 15:29:56 +0530

    fix: switch to HashRouter to fix black screen on GitHub Pages subpath deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { lazy, Suspense } from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/react";
 import "./App.css";
@@ -11,7 +11,7 @@ import { LoadingProvider } from "./context/LoadingProvider";
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <Router>
       <Routes>
         <Route
           path="/"
@@ -42,7 +42,7 @@ const App = () => {
       </Routes>
       <Analytics />
       <SpeedInsights />
-    </BrowserRouter>
+    </Router>
   );
 };
 
